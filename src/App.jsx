30dcee0392a45e10700af117fe19a3390136677f
unknown <dandarabiancas@gmail.com>
2023-08-24 20:24:09 -0300

    Organizando pastas

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import Menu from "./Assets/Components/Menu";
+import Formulario from "./Assets/Components/Formulario/index";
+import UseForm from "./Assets/Components/UseForm";
+import ReviewForm from "./Assets/Components/ReviewForm";
+import Thenks from "./Assets/Components/Thenks";
+import "./index.css";
+import Steps from "./Assets/Components/Steps";
+
+// Hooks
+import { useForm } from "./hooks/useForm";
+import { useState } from "react";
+
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  coment: "",
+}
+
+export default function App() {
+  const [data, setData] = useState(formTemplate);
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return {...prev, [key]: value};
+    })
+  }
+
+  const formComponents = [
+  <UseForm data={data} updateFieldHandler={updateFieldHandler} />,
+   <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thenks data={data} />];
+
+  const { currentStep, currentComponents, changeStep, isLastStep, isFirsStep, Steps } =
+    useForm(formComponents);
+
+  return (
+    <div className="app">
+      <Menu />
+      <useForm />
+      <Formulario
+        changeStep={changeStep}
+        currentStep={currentStep}
+        currentComponents={currentComponents}
+        isLastStep={isLastStep}
+        isFirsStep={isFirsStep}
+        Steps={Steps}
+      />   
+    </div>
+  );
+}
