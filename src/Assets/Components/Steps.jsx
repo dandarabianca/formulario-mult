7b3437f80a1894992557af5fe9901ea0fa354a46
unknown <dandarabiancas@gmail.com>
2023-08-28 19:21:49 -0300

    Formulario completo

--- src/Assets/Components/Steps.jsx
+++ src/Assets/Components/Steps.jsx
@@ -1,4 +1,5 @@
-
+import {AiOutlineUser, AiOutlineStar} from 'react-icons/ai';
+import { FiSend } from 'react-icons/fi';
 import React from "react";
 import "./Steps.css";
 
@@ -6,12 +7,15 @@ import "./Steps.css";
     return (
         <div className="steps">
             <div className="step active">
+                <AiOutlineUser />
             <span>Identificação</span>
             </div>
             <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
+                <AiOutlineStar />
             <span>Avaliação</span>
             </div>
             <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
+                <FiSend />
             <span>Envio</span>
             </div>
         </div>
